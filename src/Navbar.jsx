5e05e1795127d2b9f42fba2d2e64054df28a4c84
Shola Ayeni <ayenisholah@yahoo.com>
2019-10-30 21:46:29 +0100

    created colors design system

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
+import colors from "./colors";
 
 const NavBar = () => (
   <header
     css={css`
-      background-color: #333;
+      background-color: ${colors.secondary};
       padding: 15px;
     `}
   >
     <Link to="/">Adopt Me!</Link>
-    <span role="img" aria-label="logo">
+    <span
+      css={css`
+        font-size: 60px;
+
+        &:hover {
+          text-decoration: underline;
+        }
+      `}
+      role="img"
+      aria-label="logo"
+    >
       😎
     </span>
   </header>
